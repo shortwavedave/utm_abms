97634e4ca97ec597e58c9f52e34251481f49a4fe
VistaMarston <63620520+VistaMarston@users.noreply.github.com>
2021-07-25 15:43:47 -0600

    Update ATOC.m
    Updated Data Structures and included more layout. Still need to fill in some functions and debug the code.

--- @ATOC/ATOC.m
+++ @ATOC/ATOC.m
@@ -1,16 +1,41 @@
 classdef ATOC < handle
-    %ATOC Summary of this class goes here
-    %   Detailed explanation goes here
+    % ATOC - Monitors UAS Activity During Simulations
+    %   Monitors: Density in Lanes, Planned Routes, UAS Positions, and
+    %   Sensory Data.
+    %   Displays: Density/time graphs and Space-Time Lane Diagrams.
     
     properties
         reseverationData  % Reseveration Data (planned flight data)
-        telemetryData % Sensory/Drone Data 
-        laneData % Density Informaiton
+        telemetryData % Drone Position Data
+        laneData % lane Informaiton
         radars % Radar Information
         time % Keep track of time
     end
     
-    function aDensity = averageDensity(obj, plot, time, lanes)
+    % User Accessible Functions
+    methods
+        % Needs to be debugged
+        function obj = ATOC(res, time)
+            % ATOC - Constructor to Monitor UAS activity
+            % Input:
+            %   res (results struct)
+            %       .airways (airway struct)
+            %       .reservations (reservation struct)
+            %       .flights (flights struct)
+            %   time (float): starting time
+            obj.telemetryData = containers.Map('KeyType', 'integer', ...
+                'ValueType', 'any');
+            obj.laneData = containers.Map('KeyType', 'integer', ...
+                'ValueType', 'any');
+            obj.reseverationData = res;
+            obj.radars = LEM_radars_placement_coverage(res, range, ...
+                noise, angle);
+            obj.time = time;
+            createlaneData(res.airways);
+        end
+        
+        % Needs to Be completed
+        function aDensity = averageDensity(obj, plot, time, lanes)
             % Average Density/time Graph Creation
             if plot == 1
                 % Show the average density graph
@@ -30,6 +55,7 @@ classdef ATOC < handle
             return lanesnum/dronesnum;
         end
         
+        % Needs to Be completed
         function denseList = mostDense(obj, number, time)
             % Returns the specified number of most dense lanes
             if isEmpty(number)
@@ -39,10 +65,11 @@ classdef ATOC < handle
             if isEmpty(time)
                 % return the most dense throughout the simulation
             end
-                
+            
         end
         
-        function laneTrajectory(obj, drone, planned, actual, sensor, time)
+        % Needs to Be completed
+        function laneTrajectory(obj, planned, actual, sensor, time)
             % Plots The Drone Trajectory Information
             if ~isEmpty(planned)
                 % Plot the planned trajectory
@@ -60,6 +87,7 @@ classdef ATOC < handle
             end
         end
         
+        % Needs to Be completed
         function averDev = averageDev(obj, drones, time)
             if ~isEmpty(time)
                 % Only find it for the specfied Time interval
@@ -70,27 +98,91 @@ classdef ATOC < handle
             % different between actual and planned.
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
-        
+        % Needs to be debugged
         function handle_events(obj, src, event)
+            % handle_events - handles any listening event during simulation
             if event.EventName == "Tick"
-                % Update drone information
-                % Update sensor information
-                % 
+                obj.time = src.time;
             end
             if event.EventName == "reseveration"
-                % Update Reservation System
+                obj.reseverationData = src.reseverationData;
             end
             if event.EventName == "telemetry"
-                % Store Telemetry Data
                 id = src.id;
-                droneData = [src.x, src.v, obj.time];
-                obj.telemetryData(id).droneInfo(end + 1) = droneData;
+                flightID = src.flightNum; % Lane Index
+                droneData = [src.x, src.y, src.z, src.v];
+                laneNum = src.lane;
+                obj.updateUAS(id, flightID, droneData);
+                obj.updateLane(laneNum, src);
             end
+        end
+    end
+    
+    % Helper Private Functions
+    methods (Access = private)
+        % Needs to be debugged
+        function updateUAS(obj, id, flightID, UASData)
+            % updateUAS - updates the status of the drones
+            % Input:
+            %   id (float) - UAS Identification Number
+            %   flightID (float) - UAS flight number
+            %   UASData (1x4 array) - UAS position and speed
+            % Update the Drone Map Data - current position
+            if (~obj.telemetryData(id)) % Add UAS to MAP
+                droneInfo(flightID) = UASData;
+                obj.telemetryData(id) = droneInfo;
+            else % Update UAS Position
+                value = obj.telemetryData(id);
+                value(flightID).droneData(end + 1) = UASData;
+                obj.telemetryData(id) = value;
+            end
+        end
+        % Needs to be debugged
+        function updateLane(laneNumber, src)
+            % updateLane - updates the UAS distance along the specific lane
+            % Input:
+            %   laneNumber (float) - The Specific Lane Index
+            %   src (UAS Object) - The UAS Reporting Telemetry Data
+            value = obj.laneData(laneNumber);
+            lanes = value.pos;
+            posLanes = [lanes(4) - lanes(1), lanes(5) - lanes(2), ...
+                lanes(6) - lanes(3)];
+            posUAS = [src.x - lanes(1), src.y - lanes(2),...
+                src.z - lanes(3)];
+            dis = projectUAS(posUAS, posLanes);
+            Tnew = table(src.id, dis, obj.time);
+            value.UASInfo = [value.UASInfo; Tnew];
+            obj.laneData(laneNumber) = value;
+        end
+        % Needs to be debugged
+        function createLaneData(airways)
+        % createLaneData - initializes lane data
+        % Input:
+        %   airways (airway struct) - airway information
+            for lane = 1:size(airways.lanes, 1)
+                info = struct();
+                sz = [1 3];
+                varTypes = {'double', 'double', 'double'};
+                varNames = {'ID', 'Distance', 'Time'};
+                tnew = table('Size',sz,'VariableTypes',varTypes,...
+                    'VariableNames',varNames);
+                position = airways.lanes(lane, :);
+                info.UAS = tnew;
+                info.pos = position;
+                obj.laneData(lane) = info;
+            end
+        end
+        % Needs to be debugged
+        function dis = projectUAS(posUAS, posLane)
+            % projectUAS - locates the UAS distance along the lane
+            % Input:
+            %   posUAS (1x3 array): Vec Coord UAS Position
+            %   posLane (1x3 array): Vec Coord Lane Position
+            % Output:
+            %   dis (float): The UAS Distance Along The Lane
+            dotProd = dot(posUAS, posLane);
+            normLane = norm(posLane)^2;
+            dis = (dotProd/normLane)*posLane;
         end
     end
 end
